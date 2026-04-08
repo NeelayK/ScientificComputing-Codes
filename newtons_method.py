af45790bc286874ab63ef874ac90f22541e37274
Neelay <75205019+NeelayK@users.noreply.github.com>
2026-04-08 15:31:08 +0530

    Add files via upload

--- newtons_method.py
+++ newtons_method.py
@@ -1,26 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+MAX_ITER = 200
 TOL = 1e-8
-MAX_ITER = 20
 ALPHA_TOL = 1e-10
 MAX_ALPHA_ITER = 50
 
 def f(x):
     x1, x2 = x
-    return x1**2 + (x2**2)/4 -1
+    return x1**2 + (x2**2)/2 - 3
 
 def grad_f(x):
     x1, x2 = x
-    return np.array([2*x1, x2/2])
+    return np.array([2*x1, x2])
 
 def hessian_f(x):
     return np.array([
         [2, 0],
-        [0, 0.5]
+        [0, 1]
     ])
 
-def compute_alpha_newton(x, p):
+def compute_alpha(x, p):
     alpha = 0.0
 
     for _ in range(MAX_ALPHA_ITER):
@@ -29,8 +29,10 @@ def compute_alpha_newton(x, p):
         grad = grad_f(x_new)
         H = hessian_f(x_new)
 
+        # φ'(α)
         phi_prime = grad @ p
 
+        # φ''(α)
         phi_double_prime = p @ H @ p
 
         if abs(phi_prime) < ALPHA_TOL:
@@ -41,28 +43,31 @@ def compute_alpha_newton(x, p):
     return alpha
 
 
-x1 = float(input("x1: "))
-x2 = float(input("x2: "))
-x = np.array([x1, x2])
+x1 = float(input("Enter x1: "))
+x2 = float(input("Enter x2: "))
+x = np.array([x1, x2], dtype=float)
 
 points = [x.copy()]
 
-print("\nx-------------Iteration details-------------x\n")
+print("\n--- Modified Newton Method (2D) ---\n")
 
 for k in range(MAX_ITER):
 
-    p = -grad_f(x)
-    grad_norm = np.linalg.norm(p)
+    grad = grad_f(x)
+    grad_norm = np.linalg.norm(grad)
 
     print(f"Iteration {k}")
-    print("current position (x1,x2) =", x)
+    print("x =", x)
     print("f(x) =", f(x))
-    print("||p|| =", grad_norm)
+    print("||grad|| =", grad_norm)
 
     if grad_norm < TOL:
         break
 
-    alpha = compute_alpha_newton(x, p)
+    H = hessian_f(x)
+    p = -np.linalg.solve(H, grad)
+
+    alpha = compute_alpha(x, p)
 
     print("alpha =", alpha)
     print()
@@ -75,11 +80,10 @@ points = np.array(points)
 print("Converged to:", x)
 print("Minimum value:", f(x))
 
-
-x_vals = np.linspace(-20, 20, 1000)
-y_vals = np.linspace(-20, 20, 1000)
+x_vals = np.linspace(-10, 10, 1000)
+y_vals = np.linspace(-10, 10, 1000)
 X, Y = np.meshgrid(x_vals, y_vals)
-Z = X**2 + Y**2/4
+Z = X**2 + (Y**2)/2 - 3
 
 plt.figure()
 plt.contour(X, Y, Z, levels=50)
@@ -89,9 +93,9 @@ plt.plot(points[:,0], points[:,1], marker='o')
 plt.scatter(points[0,0], points[0,1], s=100)
 plt.scatter(0, 0, s=100)
 
-plt.title("Steepest Descent")
-plt.xlabel("x1")
-plt.ylabel("x2")
+plt.title("Modified Newton Method")
+plt.xlabel("x")
+plt.ylabel("y")
 
-plt.savefig('steepest_descent_newton_alpha.png')
+plt.savefig("modified_newton_2d.png")
 plt.show()
